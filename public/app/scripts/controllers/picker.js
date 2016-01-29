d0c7f6320298d621e416a358440b09918989f58e
bhuber2010 <bhuber@alumni.colostate.edu>
2016-01-29 15:50:53 -0700

    everything working

--- public/app/scripts/controllers/picker.js
+++ public/app/scripts/controllers/picker.js
@@ -8,18 +8,24 @@
  * Controller of the agileHoistApp
  */
 angular.module('agileHoistApp')
-  .controller('PickerCtrl', function (pickerData, lodash) {
+  .controller('PickerCtrl', function ($window, pickerData, lodash) {
     var that = this;
 
     this.heist = pickerData.heist;
+
+    this.resetHeist = function(){
+      $window.location.reload();
+    };
 
     this.runHeist = function(heistRequiredScore, heistData){
       console.log(heistRequiredScore);
       console.log(heistData);
       if (heistData.score >= heistRequiredScore) {
-        that.heist.result = "You got the loot!";
+        that.heist.result = 'You got the loot!';
+        that.heist.resultPic = 'http://www.iconarchive.com/download/i7737/hopstarter/sleek-xp-basic/Money.ico';
       } else {
-        that.heist.result = "The cops busted your ass!";
+        that.heist.result = 'The cops busted your ass!';
+        that.heist.resultPic = 'http://icons.iconseeker.com/ico/south-park-cartman/cartman-cop-zoomed.ico';
       }
 
       that.heist.run = true;
@@ -37,18 +43,19 @@ angular.module('agileHoistApp')
 
     pickerData.getHeists.then(function(data){
       that.heists = data.heists;
-    })
+    });
 
     this.confirmPick = function(role){
       var personIndex = lodash.findIndex(that.people, function(person){
         return person.name === role.pick.name;
       });
       role.pickName = role.pick.name;
+      role.pickImg = role.pick.image_url;
       that.people[personIndex].selected = true;
 
       pickerData.addToTeam(role);
       role.confirmed = true;
-      // console.log(role);
+      console.log(role);
     };
 
     this.editRole = function(role){
