56dbe49b6bfa5c852b25409c0e0b5022b495fbc3
J. Drill <JJDrill@hotmail.com>
2016-01-29 14:56:12 -0700

    added money and cartman police images

--- db/heists.js
+++ db/heists.js
@@ -3,21 +3,21 @@ var heists =
     {
       name: "Bank of America",
       address: "123 Main Street",
-      rating_required: 100,
+      rating_required: 20,
       estimated_loot: 500000,
       pivotal_tracker_url: "https://www.pivotaltracker.com/n/projects/1525919"
     },
     {
       name: "Wells Fargo Bank",
       address: "123 Bank Street",
-      rating_required: 75,
+      rating_required: 15,
       estimated_loot: 200000,
       pivotal_tracker_url: "https://www.pivotaltracker.com/n/projects/1525921"
     },
     {
       name: "U.S. Bank",
       address: "123 Money Street",
-      rating_required: 50,
+      rating_required: 10,
       estimated_loot: 100000,
       pivotal_tracker_url: "https://www.pivotaltracker.com/n/projects/1525925"
     }
